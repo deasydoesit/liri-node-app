9d46b50eb7e39409b43aef03139e04c94032edd7
deasydoesit <darwin.rinderer@gmail.com>
2018-06-06 10:06:28 -0400

    Revising control flow

--- liri.js
+++ liri.js
@@ -15,15 +15,7 @@ for (var i = 2; i < process.argv.length; i++) {
 // console.log("Hey this is my spotify object" + JSON.stringify(spotify, null, 2) + "\n\n\n\n");
 // console.log("Hey this is my client(twitter) object" +JSON.stringify(client, null, 2) + "\n\n\n\n");
 
-if (!userInputs.input2) {
-    console.log("\n\n\nHey! Provide a command! We're looking for something in the following format: \n\n\n");
-    console.log("      node liri.js <command>\n\n\n");
-    console.log("<command>'s include 'my-tweets', 'spotify-this-song', 'movie-this', and 'do-what-it-says'.\n\n\n");
-    console.log("If you pick the 'spotify-this-song' command, you also need to provide a song name, like so: \n\n\n");
-    console.log("      node liri.js spotify-this-song 'Trap Queen'\n\n\n");
-    console.log("If you pick the 'movie-this' command, you also need to provide a movie name in string format, like so: \n\n\n");
-    console.log("      node liri.js movie-this 'Avatar'\n\n\n")
-} else if (userInputs.input2 === "my-tweets") {
+if (userInputs.input2 === "my-tweets") {
     var params = {screen_name: 'busterbutt1'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
@@ -39,4 +31,12 @@ if (!userInputs.input2) {
 
 } else if (userInputs.input2 === "do-what-it-says") {
 
+} else {
+    console.log("\n\n\nHey! Provide a proper command! We're looking for something in the following format: \n\n\n");
+    console.log("      node liri.js <command>\n\n\n");
+    console.log("<command>'s include 'my-tweets', 'spotify-this-song', 'movie-this', and 'do-what-it-says'.\n\n\n");
+    console.log("If you pick the 'spotify-this-song' command, you also need to provide a song name, like so: \n\n\n");
+    console.log("      node liri.js spotify-this-song 'Trap Queen'\n\n\n");
+    console.log("If you pick the 'movie-this' command, you also need to provide a movie name in string format, like so: \n\n\n");
+    console.log("      node liri.js movie-this 'Avatar'\n\n\n");
 }
